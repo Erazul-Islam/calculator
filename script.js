06471c8600a29e7c717e1da802b02a0d4285ef1c
Erazul Islam <erazul89@gmail.com>
2023-08-22 22:13:25 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function handleclick(target){
+    document.getElementById('result').value += target;
+}
+function clearResult(){
+    document.getElementById('result').value = '';
+}
+function calculate(){
+    try{
+        const expression = document.getElementById('result').value;
+        const answer = eval(expression);
+        document.getElementById('result').value = answer;
+    }catch (error){
+        document.getElementById('result').value = 'Error'
+    }
+}
